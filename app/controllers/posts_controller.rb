ee6f8d12f738a52e59c6e04bc59ebe9fdaadc4b7
MARVIN <o.silverygold@gmail.com>
2023-12-22 21:32:23 +0300

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,11 @@
 class PostsController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 end
