892c32958cc68b83154f6cc14cfdd92719f5a48e
MARVIN <o.silverygold@gmail.com>
2023-12-23 21:57:05 +0300

    Add before action callbacks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,41 @@
 class PostsController < ApplicationController
-  def new; end
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :select_post, only: %i[show destroy edit]
 
-  def index; end
+  def new
+    @post = Post.new
+  end
+
+  def index
+    @posts = Post.paginate(page: params[:page], per_page: 9)
+  end
 
   def show; end
 
-  def create; end
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:success] = 'Post successfully created'
+      redirect_to @post
+    else
+      flash[:error] = 'Failed to build post'
+      render :new
+    end
+  end
+
+  def edit; end
 
   def destroy; end
+
+  private
+
+
+  def post_params
+    params.require(:post).permit(:title, :summary, :photo_url, :body, :category_id, tag_ids: [])
+  end
+
+  def select_post
+    @post = Post.find(params[:id])
+  end
 end
