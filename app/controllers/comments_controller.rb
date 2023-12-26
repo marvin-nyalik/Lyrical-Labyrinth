94436c0b29de7fac71d82db901ec528a636e7b65
MARVIN <o.silverygold@gmail.com>
2023-12-26 17:00:27 +0300

    Modify comments model and controller for replies recursive association

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,10 +18,31 @@ class CommentsController < ApplicationController
     end
   end
 
+  def show
+    @p_comment = Comment.includes(:replies).find(params[:id])
+    @reply = Comment.new(parent: @p_comment)
+  end
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to @post, notice: 'Comment successfully deleted'
+  end
+
+  def reply_to_comment
+    @parent_com = Comment.find(params[:id])
+    @reply = Comment.new(parent: @parent_com)
+
+    comment_params = params.require(:comment).permit(:body)
+    comment_service = CommentService.new(@parent_com, current_user, comment_params)
+    det = comment_service.create_comment_reply_notifications
+
+    if det
+      redirect_to post_comment_path(post_slug: @parent_com.post.slug, id: @parent_com.id),
+                  notice: 'Your reply was added'
+    else
+      redirect_to post_comment_path(post_slug: @parent_com.post.slug, id: @parent_com.id), alert: 'Reply not created'
+    end
   end
 
   private
