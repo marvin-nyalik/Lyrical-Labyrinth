0a91dbc6d9f6e09bceecfa6f434ef57f805b03e4
MARVIN <o.silverygold@gmail.com>
2023-12-31 23:54:46 +0300

    Define filterable concern module

---
+++ app/controllers/concerns/filterable_concern.rb
@@ -0,0 +1,21 @@
+module FilterableConcern
+  extend ActiveSupport::Concern
+
+  included do 
+    before_action :set_filtered_posts, only:[:poems, :reflections, :scars]
+  end
+
+  private
+
+  def set_filtered_posts
+    category_mapping = {
+      poems: 1,
+      reflections: 2,
+      scars: 3
+    }
+
+    category_id = category_mapping[action_name.to_sym]
+    @posts = Post.includes(:comments).where(category: category_id).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    render 'index'
+  end
+end
