8a98e3586959f188484abcacedbce1ebc3627ba1
MARVIN <o.silverygold@gmail.com>
2024-01-01 14:23:32 +0300

    Add links helper method

--- app/controllers/concerns/filterable_concern.rb
+++ app/controllers/concerns/filterable_concern.rb
@@ -1,9 +1,9 @@
 module FilterableConcern
   extend ActiveSupport::Concern
 
-  included do 
-    before_action :set_filtered_posts, only:[:poems, :reflections, :scars]
-  end
+  # included do
+  #   before_action :set_filtered_posts, only:[:poetry, :shorts, :folklore, :fairytale]
+  # end
 
   private
 
@@ -15,7 +15,9 @@ module FilterableConcern
     }
 
     category_id = category_mapping[action_name.to_sym]
-    @posts = Post.includes(:comments).where(category: category_id).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    @posts = Post.includes(:comments).where(category: category_id).order(created_at: :desc).paginate(
+      page: params[:page], per_page: 5
+    )
     render 'index'
   end
 end
