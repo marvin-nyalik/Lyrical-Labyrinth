7900815a48bcc8d02fcec2fdcdc94e446580695f
MARVIN <o.silverygold@gmail.com>
2024-01-22 11:08:03 +0300

    Add welcome alert

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,6 @@
+// app/assets/javascripts/example.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code goes here
+    console.log("Hello from example.js!");
+    alert('Welcome! Register for updates');
+});
