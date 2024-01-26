fec909fd780a03df4c77935f1222da87a960f184
MARVIN <o.silverygold@gmail.com>
2024-01-26 22:52:43 +0300

    Add base css file

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,5 +2,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Your JavaScript code goes here
     console.log("Hello from example.js!");
-    alert('Welcome! Register for updates');
 });
