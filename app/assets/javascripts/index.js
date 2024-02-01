1df051ea3bee8da6d6c4d285454c7211652b97aa
MARVIN <o.silverygold@gmail.com>
2024-02-01 13:10:57 +0300

    Add styling for mobile navbar

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,22 @@
-// app/assets/javascripts/example.js
 document.addEventListener("DOMContentLoaded", function() {
-    // Your JavaScript code goes here
-    console.log("Hello from example.js!");
+    const menubar = document.getElementById("menubar");
+    const desktopmenu = document.getElementById("desktop-menu");
+
+    menubar.addEventListener("click", (event) => {
+    event.stopPropagation();
+    desktopmenu.style.display = 'flex';
+    desktopmenu.style.zIndex = 5;
+    desktopmenu.style.height = '100vh';
+    desktopmenu.style.position = 'absolute';
+    desktopmenu.style.top = '0';
+    desktopmenu.style.left = '0';
+    desktopmenu.style.bottom = '0';
+    desktopmenu.style.right = '0';
+    desktopmenu.style.flexDirection = 'column';
+    desktopmenu.style.width = '100%';
+    desktopmenu.style.background = 'white';
+    document.body.style.overflow = 'hidden';
+    })
 });
+
+
