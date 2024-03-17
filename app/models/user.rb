c7da7409a8137c7a4dfb817c140ac726a63abef5
MARVIN <o.silverygold@gmail.com>
2024-03-17 21:37:22 +0300

    Add validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :notifications, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :full_name, presence: true
+  validates :avatar_url, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
