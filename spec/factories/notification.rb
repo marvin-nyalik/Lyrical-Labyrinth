6410153a606661497a39ff78ae0856f0a27d57e3
MARVIN <o.silverygold@gmail.com>
2024-03-17 21:39:45 +0300

    Update notification factory

--- spec/factories/notification.rb
+++ spec/factories/notification.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     
     association :user
     association :notifiable, factory: :comment
+
+    after(:build) do |notification|
+      notification.notifiable_id = notification.notifiable.id
+      notification.notifiable_type = notification.notifiable.class.to_s
+    end
   end
 end
