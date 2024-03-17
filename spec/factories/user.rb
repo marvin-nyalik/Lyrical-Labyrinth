81f586f524f6411e367398fa4749691148b84ad2
MARVIN <o.silverygold@gmail.com>
2024-03-17 21:40:11 +0300

    Register emails as factoryBot sequence

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :user do
-    email { 'johndoe@gmail.com' }
+    sequence(:email) { |n| "user#{n}@example.com" }
     full_name { 'John Doe' }
     avatar_url { 'https://facebook.com/user/avatar.jpg' }
     password { 'password' }
 
-    after(:create) { |user| user.posts << create(:post) }
-    after(:create) { |user| user.comments << create(:comment) }
+    # after(:create) { |user| user.posts << create(:post) }
+    # after(:create) { |user| user.comments << create(:comment) }
 
-    after(:create) do
-      comment = create(:comment)
-      user.notifications << create(:notification, notifiable: comment)
-    end
+    # after(:create) do
+    #   comment = create(:comment)
+    #   user.notifications << create(:notification, notifiable: comment)
+    # end
   end
 end
 
