3fb764668be095adc128455b2a50988cc3cd96cb
MARVIN <o.silverygold@gmail.com>
2024-03-26 21:44:31 +0300

    Fix rubocop

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   it 'Contains a valid factory' do
     user = FactoryBot.create(:user)
-    comment = FactoryBot.create(:comment, user: user)
+    comment = FactoryBot.create(:comment, user:)
 
-    notification = FactoryBot.build(:notification, user: user, notifiable_id: comment.id, notifiable_type: 'Comment')
+    notification = FactoryBot.build(:notification, user:, notifiable_id: comment.id, notifiable_type: 'Comment')
 
     expect(notification).to be_valid
   end
